7283bb32e19767f9c089c0e42cfb5b97ba487294
Christian <chr.koenig@outlook.com>
2021-11-12 14:49:10 +0100

    version 1.0: implemented region choice, improved plotting etc.

--- app.R
+++ app.R
@@ -1,86 +1,136 @@
 library(shiny)
 library(shinythemes)
-library(shinyWidgets)
 library(dplyr)
 library(sf)
-library(lwgeom)
+library(rnaturalearth)
 library(ggplot2)
 
 # Prevent weird behavior when cropping
 sf_use_s2(FALSE)
 
 # Load global layer of countries
-world = st_read("data/World_Countries_Boundaries/World_Countries__Generalized_.shp") %>% 
-  st_cast("POLYGON")
+world = rnaturalearth::ne_countries(returnclass = "sf")
 
-# Load valid PROJ projections strings
-projections = read.delim("data/projections.csv", header = F, sep = ";", col.names = c("code", "name")) %>% 
+# Load valid PROJ projection strings
+projections = read.delim("data/proj_valid.csv", header=F, sep=";", col.names=c("code", "name")) %>% 
+  arrange(name) %>% # Sort by name
   pull(code, name) # Cast to named vector
 
 # Front End
 ui <- fluidPage(
   # Theme
-  theme = shinytheme("simplex"),
+  theme = shinytheme("cerulean"),
   
-  # Application title
-  titlePanel("ProjectionPreview"),
-  
-  hr(),
-  
-  fluidRow(style = "background-color:#4d3a7d margin-left:50px",
+  # Layout
+  column(width = 2),
+  column(width = 8,
+         fluidRow(
+           # Application title
+           titlePanel("ProjectionPreview"),
+           p("This is a simple Shiny Application to preview geographic projections. The Code is available on my ",
+             tags$a(href = 'https://github.com/ChrKoenig/ProjectionPreview', 'Github', target = "_blank", .noWS = "outside"),
+             ".")
+         ),
+         
+         hr(),
+         
+         # Control Widgets
+         fluidRow(
            column(width=4,
-                  # PROJECTION
-                  selectizeInput("projection", label='Projection', choices = names(projections), selected = "Lat/long (Geodetic)")
+                  # Projection
+                  selectInput("projection", label='Projection', choices=names(projections), selected="Lat/long (Geodetic)"),
+                  # Region
+                  selectInput("region", label="Region", selected="World", choices=list("World", 
+                                                                                       Continent=sort(world$continent),
+                                                                                       Subregion=sort(world$subregion),
+                                                                                       Country=sort(world$name)))
            ),
            column(width=4,
-                  sliderInput("longitude", label="Longitude", min=-180, max=180, value=c(-180, 180)),
-                  sliderInput("latitude", label="Latitude", min=-90, max=90, value=c(-90, 90))
+                  # Lon/Lat Sliders
+                  sliderInput("longitude", label="Longitude", min=-180, max=180, value=c(-180, 180), step = 0.1),
+                  sliderInput("latitude", label="Latitude", min=-90, max=90, value=c(-90, 90), step = 0.1)
            ),
            column(width=3, offset=1,
-                  # DISPLAY
+                  # Display Toggles
                   tags$label("Display Settings", class="control-label"),
-                  checkboxInput("show_grid", label = "Show grid", value=T),
-                  checkboxInput("show_borders", label = "Show borders", value=T),
-                  actionButton("reset", label = "Reset values")
+                  checkboxInput("show_grid", label="Show grid", value=T),
+                  checkboxInput("show_borders", label="Show borders", value=T),
+                  actionButton("reset", label="Reset values")
            )
-  ),
-  
-  hr(),
-  # Show projected map
-  fluidRow(
-    column(width=12,
-           plotOutput("map")
-    )
+         ),
+         
+         hr(),
+         
+         # Show projected map
+         fluidRow(
+           column(width=12,
+                  plotOutput("map")
+           )
+         )
   )
 )
 
 # Server
 server <- function(input, output) {
+  scale_bbox = function(bbox, scaling=1.1){
+    ext_lon = bbox["xmax"] - bbox["xmin"]
+    ext_lat = bbox["ymax"] - bbox["ymin"]
+    scaled_bbox = bbox + c(-ext_lon*(scaling-1),
+                           -ext_lat*(scaling-1),
+                           ext_lon*(scaling-1),
+                           ext_lat*(scaling-1))
+    
+    # Make sure scaled bbox does not exceed global bbox
+    world_bbox = world %>% st_bbox()
+    values_replace = abs(scaled_bbox) > abs(world_bbox) 
+    scaled_bbox[values_replace] = world_bbox[values_replace]
+    return(scaled_bbox)
+  }
+  
+  observeEvent(input$region, handlerExpr = {
+    if(input$region %in% world$name){ # Countries
+      region_bbox = filter(world, name == input$region) %>% st_bbox() %>% scale_bbox()
+    } else if(input$region %in% world$subregion){ # Subregions
+      region_bbox = filter(world, subregion == input$region) %>% st_bbox() %>% scale_bbox()
+    } else if(input$region %in% world$continent){ # Continents
+      region_bbox = filter(world, continent == input$region) %>%  st_bbox() %>% scale_bbox()
+    } else {
+      region_bbox = world %>% st_bbox()
+    }
+    
+    updateSliderInput(inputId="longitude", value=unname(region_bbox[c("xmin","xmax")]))
+    updateSliderInput(inputId="latitude", value=unname(region_bbox[c("ymin","ymax")]))
+  })
+  
   map_crop = reactive({
-    st_crop(world, y = c(xmin=input$longitude[1], ymin=input$latitude[1], xmax=input$longitude[2], ymax=input$latitude[2]))  
+    world %>% st_crop(c(xmin = input$longitude[1],
+                                     ymin = input$latitude[1],
+                                     xmax = input$longitude[2],
+                                     ymax = input$latitude[2]))
   })
-  
-  map_dateline_split = reactive({
-    lon_split= input$lon_0 - 180
-    st_sfc(st_linestring(matrix(c(lon_split,lon_split,-90,90), nrow = 2))) %>% 
-      st_set_crs(st_crs(world))
-  })
-  
+
   map_proj = reactive({
-    map_crop() %>% 
-      st_transform(paste0(" +proj=", projections[input$projection])) %>% 
-      st_geometry()
+    map_crop() %>%
+      st_transform(paste0(" +proj=", projections[input$projection]))
   })
   
   theme_showgrid = reactive({
     if(input$show_grid){
-      theme(panel.grid.major = element_line(colour="#888888", linetype="dashed"),
-            panel.ontop = TRUE)
+      theme(panel.grid.major=element_line(colour="#888888", linetype="dashed"), panel.ontop=TRUE)
     } else {
-      theme(panel.grid.major = element_blank())
+      theme(panel.grid.major=element_blank())
     }
   })
   
+  observeEvent(input$reset, {
+    updateSelectInput(inputId="projection", selected="Lat/long (Geodetic)")
+    updateSelectInput(inputId="region", selected="World")
+    updateSliderInput(inputId="longitude", value=c(-180,180))
+    updateSliderInput(inputId="latitude", value=c(-90,90))
+    updateCheckboxInput(inputId="show_grid", value=T)
+    updateCheckboxInput(inputId="show_borders", value=T)
+  })
+
   border_color = reactive({
     if(input$show_borders){
       "grey"
@@ -88,21 +138,24 @@ server <- function(input, output) {
       "black"
     }
   })
-  
-  observeEvent(input$reset, {
-    updateSelectInput(inputId = "projection", selected = "Lat/long (Geodetic)")
-    updateSliderInput(inputId = "longitude", value = c(-180,180))
-    updateSliderInput(inputId = "latitude", value = c(-90,90))
-    updateCheckboxInput(inputId = "show_grid", value = T)
-    updateCheckboxInput(inputId = "show_borders", value = T)
+
+  fill_color = reactive({
+    map_proj = map_proj()
+    fill_color = rep("black", nrow(map_proj))
+    fill_color[map_proj$name == input$region | map_proj$subregion == input$region | map_proj$continent == input$region] = "#438ae0"
+    fill_color
   })
   
   output$map = renderPlot(
-    ggplot() +
-      geom_sf(data = map_proj(), fill = "black", col = border_color()) +
-      theme_minimal() +
-      theme_showgrid()
-  ) 
+    if(is.null(input$projection)){
+      ggplot() + theme_void()
+    } else {
+      ggplot() +
+        geom_sf(data=map_proj(), fill=fill_color(), col=border_color()) +
+        theme_minimal() +
+        theme_showgrid()
+    }
+  )
 }
 
 # Run the application 
